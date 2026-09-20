517c7687f3ee2b35c52d3355aa3c206da8250af1
MuhdAddasHamdard <muhdabbaskan@gmail.com>
2026-09-20 14:08:25 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var productName = "computer";
+
+let userName = "ali";
+var num = 34;
+var age = num;
+
+console.log(typeof age);
+console.log(typeof 34);
+console.log(typeof "ali");
+
+const dob = "2022/2/2";
+
+console.log(dob);
+dob = "2023/3/3";
